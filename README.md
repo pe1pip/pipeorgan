8d7029ffb3c1fa9402e4ce265ef12649c8835f87
Remco <remcopost@gmail.com>
2026-05-15 18:55:29 +0200

    much midi code

--- README.md
+++ README.md
@@ -14,4 +14,43 @@ The organ has 3 registers, of 42 notes each. The cade currently has hard coded t
 
 To enhance the functionality, the registers can be shifted up or down. While the code supports a single key shift, in reality one should only shift by 12.
 
-## Register on/off
+## MIDI
+
+MIDI channel 1 is used for tone on/off, MIDI channel 0 for registers (stops)
+
+### Register (stop) on/off
+
+In electronic organs it's customary to not use MIDI control change to switch stops.
+
+| key | register | shift |
+|-:|-:|-:|
+| 0 | - | - |
+| 1 | - | - |
+| 2 | 0 | -2 |
+| 3 | 0 | -1 |
+| 4 | 0 | 0 |
+| 5 | 0 | +1 |
+| 6 | 0 | +2 |
+| 7 | - | - |
+|8|-|-|
+|9|-|-|
+| 10 | 1 | -2 |
+| 11 | 1 | -1 |
+| 12 | 1 | 0 |
+| 13 | 1 | +1 |
+| 14 | 1 | +2 |
+| 15 | - | - |
+| 16 | - | - |
+| 17 | - | - |
+| 18 | 2 | -2 |
+| 19 | 2 | -1 |
+| 20 | 2 | 0 |
+| 21 | 2 | +1 |
+| 22 | 2 | +2 |
+| 23 | - | - |
+
+For each stop, the last 3 bits are ignored on the note off, so 'note off 8' throgh to 'note off 15' can be used to switch the stop off.
+
+## Control
+
+The pipes are controlled via a set of 74hc594 shift registers. These are casecaded, QH7' from the first connected to SER of the next with a maximum of 16 shift registers in series. We have the low note of the first register at the end of the 128 bit series.
--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,104 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
-#define FIRST_BYTE 128 // midi first byte has the first bit set
-
-#define KEY_OFF 0
-#define KEY_ON 16
-#define CONTROL_CHANGE 48
-#define PROGRAM_CHANGE 64
-
-#define CHANNEL 0
-
+// 74hc594 output
 #define SOUT PD2
 #define SCLK PD3
 #define RCLK PD4
 #define SCLR PD5
 #define RCLR PD6
 
-#define REGISTER_0 0 // output 0 is the first node on the first register
-#define REGISTER_1 42 // first node on second register
-#define REGISTER_2 84 // first node on third register
+#define MIDI_IN PD7
+#define MIDI_OUT PB0
 
-#define REGISTER_BASE 48
-#define REGISTER_MAX 89 // REGISTER_BASE + 41
+#define FIRST_BYTE 0x80 // midi first byte has the first bit set
+#define MIDI_CHANNEL_MASK 0x0f // low 4 bits are the channel
+
+#define KEY_OFF 0
+#define KEY_ON 16
+#define CONTROL_CHANGE 48
+#define PROGRAM_CHANGE 64
+
+#define STOP_CHANNEL 0
+#define KEY_CHANNEL 1
+
+#define MIDI_IDLE 0
+#define MIDI_COMMAND_RECEIVED 1
+#define MIDI_DATA0_RECEIVED 2
+#define MIDI_DATA1_RECEIVED 3
+
+#define STOP_0 0 // output 0 is the first node on the first stop
+#define STOP_1 42 // first node on second stop
+#define STOP_2 84 // first node on third stop
+
+#define STOP_BASE 48 // C3 is our first note
+#define STOP_MAX 89 // STOP_BASE + 41
+
+SoftwareSerial midi(MIDI_IN, MIDI_OUT);
 
 // function declarations
-void setTone(unsigned char tone, unsigned char state);
+void initOrgan ();
+void initSerial ();
+void setTone (uint8_t tone, uint8_t state);
 void send ();
-unsigned char keyToTone (unsigned char key, unsigned char reg);
-boolean checkKey (unsigned char key);
+uint8_t keyToTone (uint8_t key, uint8_t stop);
+boolean checkKey (uint8_t key);
+void doStop ();
+void doKey ();
 
 // the output buffer, 128 bits
-unsigned char out[7];
+uint8_t out[7];
 
-// the shift on the three registers
-char register_0_shift = 0;
-char register_1_shift = 0;
-char register_2_shift = 0;
+// the shift on the three stops
+uint8_t stop_0_shift = 0;
+uint8_t stop_1_shift = 0;
+uint8_t stop_2_shift = 0;
 
-boolean register_0_playing = false;
-boolean register_1_playing = false;
-boolean register_2_playing = false;
+boolean stop_0_playing = false;
+boolean stop_1_playing = false;
+boolean stop_2_playing = false;
 
-void setup() {
+uint8_t midi_state;
+uint8_t midi_buffer[3];
+
+void setup () {
+  initOrgan();
+  initSerial();
+}
+
+// loop over all 3 stops, play all 42 keys
+void loop () {
+  for (uint8_t i = STOP_BASE; i <= STOP_MAX; i++) {
+    uint8_t j = keyToTone(i, STOP_0);
+    setTone(j, KEY_ON);
+    send();
+    setTone(j, KEY_OFF);
+    delay(500);
+  }
+  for (uint8_t i = STOP_BASE; i <= STOP_MAX; i++) {
+    uint8_t j = keyToTone(i, STOP_1);
+    setTone(j, KEY_ON);
+    send();
+    setTone(j, KEY_OFF);
+    delay(500);
+  }
+  for (uint8_t i = STOP_BASE; i <= STOP_MAX; i++) {
+    uint8_t j = keyToTone(i, STOP_2);
+    setTone(j, KEY_ON);
+    send();
+    setTone(j, KEY_OFF);
+    delay(500);
+  }
+}
+
+void initOrgan () {
   pinMode(SOUT, OUTPUT);
   pinMode(SCLK, OUTPUT);
   pinMode(RCLK, OUTPUT);
   pinMode(SCLR, OUTPUT);
   pinMode(RCLR, OUTPUT);
   
-  for (unsigned char i=0; i<7; i++) {
+  for (uint8_t i=0; i<7; i++) {
     out[i] = 0;
   }
   digitalWrite(SCLR, LOW);
@@ -57,35 +108,15 @@ void setup() {
   digitalWrite(RCLR, HIGH);
 }
 
-// loop over all 3 registers, play all 42 keys
-void loop() {
-  for (unsigned char i = REGISTER_BASE; i <= REGISTER_MAX; i++) {
-    unsigned char j = keyToTone(i, REGISTER_0);
-    setTone(j, KEY_ON);
-    send();
-    setTone(j, KEY_OFF);
-    delay(500);
-  }
-  for (unsigned char i = REGISTER_BASE; i <= REGISTER_MAX; i++) {
-    unsigned char j = keyToTone(i, REGISTER_1);
-    setTone(j, KEY_ON);
-    send();
-    setTone(j, KEY_OFF);
-    delay(500);
-  }
-  for (unsigned char i = REGISTER_BASE; i <= REGISTER_MAX; i++) {
-    unsigned char j = keyToTone(i, REGISTER_2);
-    setTone(j, KEY_ON);
-    send();
-    setTone(j, KEY_OFF);
-    delay(500);
-  }
+void initSerial () {
+  midi.begin(31250);
+  midi_state = MIDI_IDLE;
 }
 
 // set or clear the bit in the output buffer that corrosponds with the tone
-void setTone (unsigned char tone, unsigned char state) {
-  unsigned char bit = 1 >> tone & 0x0F;
-  unsigned char byte = (tone >> 4) && 0x07;
+void setTone (uint8_t tone, uint8_t state) {
+  uint8_t bit = 1 >> tone & 0x0F;
+  uint8_t byte = (tone >> 4) && 0x07;
   if (state == KEY_ON) {
     out[byte] = out[byte] | bit;
   }
@@ -94,17 +125,17 @@ void setTone (unsigned char tone, unsigned char state) {
   }
 }
 
-// set the output buffer to the shift registers, 128 bits
+// set the output buffer to the shift stops, 128 bits
 void send () {
   digitalWrite(RCLK, LOW);
-  digitalWrite(SCLR, LOW); // clear the serial register
+  digitalWrite(SCLR, LOW); // clear the serial stop
   delayMicroseconds(10);
-  digitalWrite(SCLR, HIGH); // enable serial register
+  digitalWrite(SCLR, HIGH); // enable serial stop
   delayMicroseconds(10);
   for (int i=0; i<7; i++) {
-    unsigned char currentByte = out[i];
+    uint8_t currentByte = out[i];
     for (int j=0; j<8; j++) {
-      unsigned char currentBit = currentByte && 0x1;
+      uint8_t currentBit = currentByte && 0x1;
       currentByte >>= 1;
       digitalWrite(SCLK, LOW);
       delayMicroseconds(10);
@@ -116,10 +147,46 @@ void send () {
   digitalWrite(RCLK, HIGH);
 }
 
-unsigned char keyToTone (unsigned char key, unsigned char reg) {
-  return key - REGISTER_BASE + reg;
+uint8_t keyToTone (uint8_t key, uint8_t stop) {
+  return key - STOP_BASE + stop;
 }
 
-boolean checkKey (unsigned char key) {
-  return key >= REGISTER_BASE && key <= REGISTER_MAX;
+boolean checkKey (uint8_t key) {
+  return key >= STOP_BASE && key <= STOP_MAX;
+}
+
+void doSerial () {
+  int bytes = midi.available();
+  if (bytes > 0) {
+    uint8_t b = midi.read();
+    if (b & FIRST_BYTE) { // if we 
+      midi_buffer[MIDI_IDLE] = b;
+      midi_state = MIDI_COMMAND_RECEIVED;
+      return;
+    } else {
+      if (midi_state != MIDI_IDLE) {
+        midi_buffer[midi_state] = b;
+        midi_state += 1;
+      }
+    }
+    if (midi_state == MIDI_DATA1_RECEIVED) {
+      /* once 3 bytes have been received
+       * - reset state
+       * - check channel
+       */
+      midi_state = MIDI_IDLE;
+      uint8_t channel = midi_buffer[0] & MIDI_CHANNEL_MASK;
+      midi_buffer[0] = midi_buffer[0] & 0x70; // high byte and channel bytes are not impor
+      switch (channel) {
+        case STOP_CHANNEL:
+          doStop();
+          break;
+        case KEY_CHANNEL:
+          doKey();
+          break;
+        default:
+          // we don't know about this channel, data ignored
+      }
+    }
+  }
 }
